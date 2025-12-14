4c9f625a95572fe1aa0fd79a1893c4ff31a74eb8
Atharva Waghmare <atharvawaghmare848@gmail.com>
2025-12-14 18:29:02 +0530

    Add User Api Added

--- Backend/routes/admin/users.js
+++ Backend/routes/admin/users.js
@@ -16,8 +16,26 @@ router.get('/', (req, res) => {
         SELECT u.user_id, u.name, u.email, u.phone, u.is_active, r.role_name
         FROM users u
         JOIN roles r ON u.role_id = r.role_id`
-        
+
     pool.query(sql, (err, data) => {
         res.send(result.createResult(err, data))
+    })
+})
+
+
+// ADMIN : Add New User/admin
+
+router.post('/', (req, res) => {
+    const { name, email, password, phone, role_id } = req.body
+    const sql = `INSERT INTO users(name, email, password_hash, phone, role_id, is_active) VALUES (?,?,?,?,? , 1)`
+    
+    bcrypt.hash(password, SaltRounds, (err, password_hash) => {
+        if (password_hash) {
+            pool.query(sql, [name, email, password_hash, phone, role_id], (err, data) => {
+                res.send(result.createResult(err, data))
+            })
+        } else {
+            res.send(result.createResult(err))
+        }
     })
 })
